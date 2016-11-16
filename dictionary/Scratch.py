eb7f66da5007c82405d1e73d305c0ad0bcd13814
Duluri\Ryan <ryanoftinellb@gmail.com>
2016-11-16 23:22:01 +1100

    Added hyperlinks between dictionary entries; changed background colour

--- dictionary/Scratch.py
+++ dictionary/Scratch.py
@@ -1,18 +1,9 @@
 with open('Dictionary.txt', 'r') as f:
-    with open('../index.html', 'w') as g:
-        new_text = "<head>\n<title>Lulani Wordlist</title>\n"
-        new_text += '<link rel="stylesheet" type="text/css" href="wordlist.css">\n'
-        new_text += "<body>\n<ul>"
-        g.write(new_text)
+    with open('Replacement.txt', 'w') as g:
         line = f.readline()
         while line != "":
-            if line[0:3] == "[1]":
-                word = line[3:-1]
-                hyperlink_title = word.replace("&#x2019;", "'")
-                hyperlink_title = hyperlink_title.replace("&#x294;", "''")
-                new_text = '<li><a href="dictionary/' + hyperlink_title + '.html">'
-                new_text += word + "</a><br>\n"
-                g.write(new_text)
+            if line[0:3] == "[3]":
+                line = line.replace("<", "&lt;")
+                line = line.replace(">", "&gt;")
+            g.write(line)
             line = f.readline()
-        new_text = "</ul></body>\n"
-        g.write(new_text)
