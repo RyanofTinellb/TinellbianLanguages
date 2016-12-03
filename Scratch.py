4c871267060481b0fd015ee11b27e22851147f48
Duluri\Ryan <ryanoftinellb@gmail.com>
2016-12-04 08:50:07 +1100

    minor

--- Scratch.py
+++
@@ -1,7 +0,0 @@
-k = "dictionary_data.txt"
-with open(k, "r") as f:
-    text = f.read()
-for i in "aiu":
-    text = text.replace("href=\"../'" + i, "href=\"../" + i + "/'" + i)
-with open(k, "w") as f:
-    f.write(text)
