50d8b717f728d54d9b0829c1815f5c42279a5fef
Duluri\Ryan <ryanoftinellb@gmail.com>
2016-12-21 10:57:41 +1100

    Made it so that when searching for parts of speech, one can tell what each word means.

--- Scratch.py
+++ Scratch.py
@@ -1,7 +1,15 @@
 page = ""
+next_line = False
 with open("dictionary_data.txt", "r") as f:
     for line in f:
-        page += line + (line[:3] == "[6]") * "<span class=\"hidden\"></span>"
+        if line[:3] == "[6]":
+            line = line[:-1] + "<div class=\"definition\">"
+            next_line = True
+        elif next_line:
+            next_line = False
+            line = line[:-1] + "</div>\n"
+        page += line
+
 with open("dictionary_data.txt", "w") as g:
     g.write(page)
 
