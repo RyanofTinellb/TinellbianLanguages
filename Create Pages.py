97030cfcc3de956eb34d424e52d50c4c21659fc9
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-01-20 00:15:46 +1100

    no message

--- Create Pages.py
+++
@@ -1,7 +0,0 @@
-from HtmlPage import *
-
-HtmlPage("grammar", 3)
-HtmlPage("story", 3)
-HtmlPage("dictionary", 2)
-# create_search()
-
--- scratch2.py
+++
@@ -1,3 +0,0 @@
-k = [4, 63, 7]
-l = [2, 6, 7]
-print(list(enumerate(zip(k,l))))
