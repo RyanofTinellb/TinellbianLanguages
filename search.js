8b7ff1e3b5123bedacb1f099789add4318874314
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-11-08 14:35:58 +1100

    Refactor search.js

--- search.js
+++ search.js
@@ -1,23 +1,20 @@
-
-
 if (window.location.href.indexOf("?") != -1) {
 	search();
 }
 
 function search() {
-	document.getElementById("results").innerHTML = "<ul><li>Searching...</li></ul>";
+	document.getElementById("results").innerHTML = "Searching...";
 	var url = "searching.json";
 	var xmlhttp = new XMLHttpRequest();
-	var andButton = document.getElementById("and");
+	var andButton = document.getElementById("and")
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var text = JSON.parse(this.responseText);
 			var terms = getTerms();
-			if (terms.length == 1) {arr = oneTermSearch(text, terms);}
-			else {
-				if (andButton.checked) {arr = andSearch(text, terms);}
-				else {arr = orSearch(text, terms);}
-			}
+      if (!terms.length) {arr = [];}
+			else if (terms.length == 1) {arr = oneTermSearch(text, terms);}
+			else if (andButton.checked) {arr = andSearch(text, terms);}
+			else {arr = orSearch(text, terms);}
 			display(arr, text, "results", terms);
 		}
 	};
@@ -27,19 +24,27 @@ function search() {
 
 // returns array of terms
 function getTerms() {
-	var markup = ["%E2%80%99", "'", "%c3%bb", "$u", "%27", "'", "\u0294", "''", "\u00ec", "$e", "%29", ")", "%c5%97", ",r",	"%20", "+", "%24", "$", "%25", "%",
+	var andOr;
+	var url;
+	var searchString;
+	var text;
+	const MARKUP = ["%E2%80%99", "'", "%c3%bb", "$u", "%27", "'", "\u0294", "''", "\u00ec", "$e", "%29", ")", "%c5%97", ",r",	"%20", "+", "%24", "$", "%25", "%",
 	"%3b", " ", "%2cr", ",r"];
-	var url = window.location.href;
+	url = window.location.href;
 	url  = url.split("?");
-	var searchString = url[1].split("&");
-	var andOr = searchString[1].split("=")[1];
+	searchString = url[1].split("&");
+	try {
+		andOr = searchString[1].split("=")[1];
+	} catch (err) {
+		andOr = "and"
+	}
 	if (andOr == "or") {document.getElementById("or").checked = true}
-	var text = searchString[0].split("=")[1];
-	for (i = 0; i < markup.length; i += 2) {
-		text = text.split(markup[i]).join(markup[i+1]).toLowerCase();
+	text = searchString[0].split("=")[1];
+	for (i = 0; i < MARKUP.length; i += 2) {
+		text = text.split(MARKUP[i]).join(MARKUP[i+1]).toLowerCase();
 	}
 	document.getElementById("term").value = text.split("+").join(" ");
-	return text.split("+").filter(function (i) {return i != "";});
+	return text.split("+").filter(i => i != "");
 }
 
 // builds array of results containing any search term
@@ -54,15 +59,12 @@ function orSearch(arr, terms) {
 			output.push(results[r])
 		}
 	}
-	output.sort(function (a,b) {return parseInt(a[0]) >= parseInt(b[0]);})
+	output.sort((a,b) => parseInt(a[0]) >= parseInt(b[0]));
 	var i = 0;
 	while (i < output.length - 1) {
 		if (output[i][0] == output[i+1][0]) {
-			output[i][1] = output[i][1].concat(output[i+1][1]).sort(function (a,b) {
-				return a < b;}).filter(function(item, pos, ary) {
-			return !pos || item != ary[pos - 1];
-				})
-			output = output.filter(function(item, pos, ary) {return pos != i;});
+			output[i][1] = output[i][1].concat(output[i+1][1]).sort((a,b) => a < b).filter((item, pos, ary) => !pos || item != ary[pos - 1]);
+			output = output.filter((item, pos, ary) => pos != i);
 		} else {i++;}
 	}
 	return output;
@@ -79,19 +81,19 @@ function andSearch(arr, terms) {
 			output.push(results[r])
 		}
 	}
-	output.sort(function (a,b) {return parseInt(a[0]) >= parseInt(b[0]);})
+	output.sort((a,b) => parseInt(a[0]) >= parseInt(b[0]))
 	for (var i = num; i < output.length; i++) {
 		if (output[i][0] == output[i - num][0]) {
 			for (j = i - num; j < i; j++) {
 				output[i][1] = output[i][1].concat(output[j][1]);
 			}
-			output[i][1] = output[i][1].sort(function (a,b) {
-				return a >= b;}).filter(function (item, pos, ary) {
-					return (!pos || item != ary[pos - 1]);
-			});
+			output[i][1] = output[i][1].sort((a,b) => a >= b).
+          filter((item, pos, ary) => !pos || item != ary[pos - 1]);
 		}
 	}
-	output = output.filter(function (item, pos, ary) {return (pos >= num && item[0] == ary[pos - num][0]);});
+	output = output.filter(
+      (item, pos, ary) => pos >= num && item[0] == ary[pos - num][0]
+    );
 	return output;
 }
 
@@ -114,25 +116,24 @@ function capitalise(string) {
 }
 
 function markdown(arr) {
-	var marking = ["$a", "&acirc;", "$e", "&ecirc;", "$i", "&icirc;", "$o", "&ocirc;", "$u", "&ucirc;", "$e", "&ecirc;", "$a", "&acirc;", "$e", "&ecirc;", ")a", "&agrave;", ")e", "&egrave;", ")i", "&igrave;", ")o", "&ograve;", ")u", "&ugrave;", "_o", "&#x14d;", "+h", "&#x2b0;", ",c", "&#x255;", ",n",  "&#x14b;", "''", "&#x294;", "'", "&rsquo;", "$h", "&#x2b1;", "-i", "&#x268;", "=j", "&#x25f;", "$l", "&#x28e;", "$n", "&#x272;", "$r", "&#x279;", ",r", "&#x157;", "!e", "&#x259;", "-u", "&#x289;", "_u", "&#x16b;"]
+	const MARKING = ["$a", "&acirc;", "$e", "&ecirc;", "$i", "&icirc;", "$o", "&ocirc;", "$u", "&ucirc;", "$e", "&ecirc;", "$a", "&acirc;", "$e", "&ecirc;", ")a", "&agrave;", ")e", "&egrave;", ")i", "&igrave;", ")o", "&ograve;", ")u", "&ugrave;", "_o", "&#x14d;", "+h", "&#x2b0;", ",c", "&#x255;", ",n", "&#x14b;", "'", "&rsquo;", "''", "&#x294;", "$h", "&#x2b1;", "-i", "&#x268;", "=j", "&#x25f;", "$l", "&#x28e;", "$n", "&#x272;", "$r", "&#x279;", ",r", "&#x157;", "!e", "&#x259;", "-u", "&#x289;", "_u", "&#x16b;"]
 	var terms = new Array;
 	for (termnum in arr) {
 		var term = arr[termnum];
-		for (i = 0; i < marking.length; i++) {
-			term = term.split(marking[i]).join(marking[++i]);
+		for (i = 0; i < MARKING.length; i++) {
+			term = term.split(MARKING[i]).join(MARKING[++i]);
 		}
 		terms.push(term);
 	}
 	return terms;
 }
 
-
 // displays results as list
 // @param Array arr: results array
 function display(arr, data, id, terms) {
 	terms = markdown(terms);
-	if (arr.length == 0) {
-		document.getElementById(id).innerHTML = "<ul><li>Search term(s) not found</li></ul>";
+	if (!arr.length) {
+		document.getElementById(id).innerHTML = "Search term(s) not found";
 		return;
 	}
 	var text = "<ol>"
