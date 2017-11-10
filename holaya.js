8c9b4e307789386106548494dced525b93da0431
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-11-10 19:06:47 +1100

    Add vowels to script
    As long as the input string strictly
    follows (CV)* order, this will
    format fine.

--- holaya.js
+++ holaya.js
@@ -1,3 +1,7 @@
+var foreground_colour = 'black';
+var background_colour = 'powderblue';
+
+
 var letterB = ['***',
                '* *',
                '  *',
@@ -71,7 +75,7 @@ var letterR = ['* *',
                ' * ',
                '***'];
 
-var letterS = ['***',
+var letterS = [' * ',
                '* *',
                '  *',
                ' * ',
@@ -107,67 +111,95 @@ var letterZ = ['***',
                ' * ',
                '***'];
 
+var letterA = [' ', ' ', ' ', ' ', ' '];
+var letterE = ['*', ' ', ' ', ' ', ' '];
+var letterI = ['*', '*', ' ', ' ', ' '];
+var letterO = ['*', ' ', '*', '*', ' '];
+var letterU = ['*', '*', ' ', '*', ' '];
+
 var letters = [];
 letters['B'] = letterB;
-letters['b'] = letterB;
 letters['C'] = letterC;
-letters['c'] = letterC;
 letters['D'] = letterD;
-letters['d'] = letterD;
 letters['F'] = letterF;
-letters['f'] = letterF;
 letters['G'] = letterG;
-letters['g'] = letterG;
 letters['H'] = letterH;
-letters['h'] = letterH;
 letters['K'] = letterK;
-letters['k'] = letterK;
 letters['L'] = letterL;
-letters['l'] = letterL;
 letters['M'] = letterM;
-letters['m'] = letterM;
 letters['N'] = letterN;
-letters['n'] = letterN;
 letters['P'] = letterP;
-letters['p'] = letterP;
 letters['R'] = letterR;
-letters['r'] = letterR;
 letters['S'] = letterS;
-letters['s'] = letterS;
 letters['T'] = letterT;
-letters['t'] = letterT;
 letters['V'] = letterV;
-letters['v'] = letterV;
 letters['W'] = letterW;
-letters['w'] = letterW;
 letters['Y'] = letterY;
-letters['y'] = letterY;
 letters['Z'] = letterZ;
-letters['z'] = letterZ;
+
+letters['A'] = letterA;
+letters['E'] = letterE;
+letters['I'] = letterI;
+letters['O'] = letterO;
+letters['U'] = letterU;
 
 function write(word) {
   let output = '';
-  for (letter of word) {
-    output += display(letters[letter], letter);
+  for (let i = 0; i < text.length + 1; i += 2) {
+    output += syllable(text.substring(i, i + 2));
   }
   return output;
 }
 
+// combines two character arrays into one
+function combine(letter1, letter2) {
+  if (!letter2) {
+    return letter1;
+  }
+  if (!letter1) {
+    return letter2;
+  }
+  let length = Math.min(letter1.length, letter2.length);
+  let combine = [];
+  for (let i = 0; i < length; i++) {
+    combine.push(letter1[i] + ' ' + letter2[i])
+  }
+  return combine;
+}
+
+// displays an HTML table displaying the syllable
+function syllable(string) {
+  chars = string.toUpperCase();
+  if (chars.length == 1) {
+    return display(letters[chars], string);
+  } else if (chars.length == 2){
+    let letter = combine(letters[chars[0]], letters[chars[1]]);
+    return display(letter, string);
+  } else {
+    return '';
+  }
+}
+
 function display(letter, character) {
+  if (character.length == 1) {
+    var td = '<td></td>';
+  } else {
+    var td = '';
+  }
   if (letter) {
-    let display = '<table style="display:inline-table;"><tr><td colspan="5" style="text-align:center;">' + character + '</td></tr>';
+    let display = '<table style="display:inline-table;"><tr><td colspan="7" style="text-align:center;font-family:monospace;">' + character + '</td></tr>';
     for (row of letter) {
-      display += '<tr><td></td>';
+      display += '<tr>' + td;
       for (column of row) {
         display += '<td style="background-color:'
         if (column == '*') {
-          display += 'black';
+          display += foreground_colour;
         } else {
-          display += 'powderblue';
+          display += background_colour;
         }
         display += ';"></td>';
       }
-      display += '<td></td></tr>';
+      display += td + td + td + '</tr>';
     }
     display += '</table>&nbsp;';
     return display;
@@ -178,7 +210,7 @@ function display(letter, character) {
 
 function glossary() {
   glossary = '';
-  let letters = 'bcdfghklmnprstvwyz';
+  let letters = 'BCDFGHKLMNPRSTVWYZ';
   let descriptions = ['bat', 'creature', 'dog tail', 'fence', 'gold medal', 'half', 'king&rsquo;s throne', 'large', 'my back', 'no king', 'person looking down a microscope', 'rabbit', 'squiggle', '(spinning) top', 'vacuum', 'woman with aching back', 'yell', 'zebra stripes'];
   for (let i = 0; i < letters.length; i++) {
     glossary += '<p>' + write(letters[i]) + ': ' + descriptions[i] + '</p>';
