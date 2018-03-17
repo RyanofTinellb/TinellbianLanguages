44d6e01a947e1fe2eb3a7df2e08e9d7f2ccf8585
Ryan Eakins <ryanoftinellb@gmail.com>
2018-03-17 14:39:11 +1100

    Move files to subfolders

--- 404Search.js
+++ 404Search.js
@@ -1,162 +1,220 @@
 if (window.location.href.indexOf("?") != -1) {
-	search();
+  search();
+} else {
+  searchForTerm();
+}
+
+function searchForTerm() {
+  document.getElementById("results").innerHTML = "<ul><li>Searching...</li></ul>";
+  var url = "/searching.json";
+  var xmlhttp = new XMLHttpRequest();
+  var andButton = document.getElementById("and");
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var text = JSON.parse(this.responseText);
+      var terms = getTermfrom404().split("%20");
+      if (terms.length == 1) {
+        arr = oneTermSearch(text, terms);
+      } else {
+        arr = andSearch(text, terms);
+      }
+      display(arr, text, "results", terms);
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function getTermfrom404() {
+  /* searches for the term after the final slash of the url, without the .html ending */
+  var url = window.location.href;
+  var terms = url.split("/");
+  var term = terms[terms.length - 1]
+  try {
+    term = term.split(".html")[0];
+  } catch (err) {}
+  return term
 }
 
 function search() {
-	document.getElementById("results").innerHTML = "Searching...";
-	var url = "/searching.json";
-	var xmlhttp = new XMLHttpRequest();
-	var andButton = document.getElementById("and")
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var text = JSON.parse(this.responseText);
-			var terms = getTerms();
-      if (!terms.length) {arr = [];}
-			else if (terms.length == 1) {arr = oneTermSearch(text, terms);}
-			else if (andButton.checked) {arr = andSearch(text, terms);}
-			else {arr = orSearch(text, terms);}
-			display(arr, text, "results", terms);
-		}
-	};
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
+  document.getElementById("results").innerHTML = "<ul><li>Searching...</li></ul>";
+  var url = "/searching.json";
+  var xmlhttp = new XMLHttpRequest();
+  var andButton = document.getElementById("and");
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var text = JSON.parse(this.responseText);
+      var terms = getTerms();
+      if (terms.length == 1) {
+        arr = oneTermSearch(text, terms);
+      } else {
+        if (andButton.checked) {
+          arr = andSearch(text, terms);
+        } else {
+          arr = orSearch(text, terms);
+        }
+      }
+      display(arr, text, "results", terms);
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
 }
 
 // returns array of terms
 function getTerms() {
-	var andOr;
-	var url;
-	var searchString;
-	var text;
-	const MARKUP = ["%E2%80%99", "'", "%c3%bb", "$u", "%27", "'", "\u0294", "''", "\u00ec", "$e", "%29", ")", "%c5%97", ",r",	"%20", "+", "%24", "$", "%25", "%",
-	"%3b", " ", "%2cr", ",r"];
-	url = window.location.href;
-	url  = url.split("?");
-	searchString = url[1].split("&");
-	try {
-		andOr = searchString[1].split("=")[1];
-	} catch (err) {
-		andOr = "and"
-	}
-	if (andOr == "or") {document.getElementById("or").checked = true}
-	text = searchString[0].split("=")[1];
-	for (i = 0; i < MARKUP.length; i += 2) {
-		text = text.split(MARKUP[i]).join(MARKUP[i+1]).toLowerCase();
-	}
-	document.getElementById("term").value = text.split("+").join(" ");
-	return text.split("+").filter(i => i != "");
+  var markup = ["%E2%80%99", "'", "%c3%bb", "$u", "%27", "'", "\u0294", "''", "\u00ec", "$e", "%28", "(", "%29", ")", "%c5%97", ",r", "%20", "+", "%24", "$", "%25", "%",
+    "%3b", " ", "%2cr", ",r"
+  ];
+  var url = window.location.href;
+  url = url.split("?");
+  var searchString = url[1].split("&");
+  try {
+    andOr = searchString[1].split("=")[1];
+  } catch (err) {
+    andOr = "and"
+  }
+  if (andOr == "or") {
+    document.getElementById("or").checked = true
+  }
+  var text = searchString[0].split("=")[1];
+  for (i = 0; i < markup.length; i += 2) {
+    text = text.split(markup[i]).join(markup[i + 1]).toLowerCase();
+  }
+  document.getElementById("term").value = text.split("+").join(" ");
+  return text.split("+").filter(function(i) {
+    return i != "";
+  });
 }
 
 // builds array of results containing any search term
 // @param arr: raw json array (usually from file)
 // @param String[] terms: search terms
 function orSearch(arr, terms) {
-	var output = new Array;
-	for (t in terms) {
-		var term = terms[t];
-		results = oneTermSearch(arr, term);
-		for (r in results) {
-			output.push(results[r])
-		}
-	}
-	output.sort((a,b) => parseInt(a[0]) >= parseInt(b[0]));
-	var i = 0;
-	while (i < output.length - 1) {
-		if (output[i][0] == output[i+1][0]) {
-			output[i][1] = output[i][1].concat(output[i+1][1]).sort((a,b) => a < b).filter((item, pos, ary) => !pos || item != ary[pos - 1]);
-			output = output.filter((item, pos, ary) => pos != i);
-		} else {i++;}
-	}
-	return output;
+  var output = new Array;
+  for (t in terms) {
+    var term = terms[t];
+    results = oneTermSearch(arr, term);
+    for (r in results) {
+      output.push(results[r])
+    }
+  }
+  output.sort(function(a, b) {
+    return parseInt(a[0]) >= parseInt(b[0]);
+  })
+  var i = 0;
+  while (i < output.length - 1) {
+    if (output[i][0] == output[i + 1][0]) {
+      output[i][1] = output[i][1].concat(output[i + 1][1]).sort(function(a, b) {
+        return a < b;
+      }).filter(function(item, pos, ary) {
+        return !pos || item != ary[pos - 1];
+      })
+      output = output.filter(function(item, pos, ary) {
+        return pos != i;
+      });
+    } else {
+      i++;
+    }
+  }
+  return output;
 }
 
 // builds array of results containing all search terms
 function andSearch(arr, terms) {
-	var output = new Array;
-	num = terms.length - 1;
-	for (t in terms) {
-		var term = terms[t];
-		results = oneTermSearch(arr, term);
-		for (r in results) {
-			output.push(results[r])
-		}
-	}
-	output.sort((a,b) => parseInt(a[0]) >= parseInt(b[0]))
-	for (var i = num; i < output.length; i++) {
-		if (output[i][0] == output[i - num][0]) {
-			for (j = i - num; j < i; j++) {
-				output[i][1] = output[i][1].concat(output[j][1]);
-			}
-			output[i][1] = output[i][1].sort((a,b) => a >= b).
-          filter((item, pos, ary) => !pos || item != ary[pos - 1]);
-		}
-	}
-	output = output.filter(
-      (item, pos, ary) => pos >= num && item[0] == ary[pos - num][0]
-    );
-	return output;
+  var output = new Array;
+  num = terms.length - 1;
+  for (t in terms) {
+    var term = terms[t];
+    results = oneTermSearch(arr, term);
+    for (r in results) {
+      output.push(results[r])
+    }
+  }
+  output.sort(function(a, b) {
+    return parseInt(a[0]) >= parseInt(b[0]);
+  })
+  for (var i = num; i < output.length; i++) {
+    if (output[i][0] == output[i - num][0]) {
+      for (j = i - num; j < i; j++) {
+        output[i][1] = output[i][1].concat(output[j][1]);
+      }
+      output[i][1] = output[i][1].sort(function(a, b) {
+        return a >= b;
+      }).filter(function(item, pos, ary) {
+        return (!pos || item != ary[pos - 1]);
+      });
+    }
+  }
+  output = output.filter(function(item, pos, ary) {
+    return (pos >= num && item[0] == ary[pos - num][0]);
+  });
+  return output;
 }
 
 function oneTermSearch(arr, term) {
-	text = new Array;
-	for (page in arr.terms[term]) {
-		text.push([page, arr.terms[term][page]])
-	}
-	return text;
+  text = new Array;
+  for (page in arr.terms[term]) {
+    text.push([page, arr.terms[term][page]])
+  }
+  return text;
 }
 
 // capitalises first letter
 function capitalise(string) {
-	if (string.length == 0) {return ""}
-	if (string.startsWith("&rsquo;")) {
-		return string.replace("&rsquo;", "&#x294;");
-	} else {
-		return string.charAt(0).toUpperCase() + string.slice(1);
-	}
+  if (string.length == 0) {
+    return ""
+  }
+  if (string.startsWith("&rsquo;")) {
+    return string.replace("&rsquo;", "&#x294;");
+  } else {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 }
 
 function markdown(arr) {
-	const MARKING = ["$a", "&acirc;", "$e", "&ecirc;", "$i", "&icirc;", "$o", "&ocirc;", "$u", "&ucirc;", "$e", "&ecirc;", "$a", "&acirc;", "$e", "&ecirc;", ")a", "&agrave;", ")e", "&egrave;", ")i", "&igrave;", ")o", "&ograve;", ")u", "&ugrave;", "_o", "&#x14d;", "+h", "&#x2b0;", ",c", "&#x255;", ",n", "&#x14b;", "'", "&rsquo;", "''", "&#x294;", "$h", "&#x2b1;", "-i", "&#x268;", "=j", "&#x25f;", "$l", "&#x28e;", "$n", "&#x272;", "$r", "&#x279;", ",r", "&#x157;", "!e", "&#x259;", "-u", "&#x289;", "_u", "&#x16b;"]
-	var terms = new Array;
-	for (termnum in arr) {
-		var term = arr[termnum];
-		for (i = 0; i < MARKING.length; i++) {
-			term = term.split(MARKING[i]).join(MARKING[++i]);
-		}
-		terms.push(term);
-	}
-	return terms;
+  var marking = ["$a", "&acirc;", "$e", "&ecirc;", "$i", "&icirc;", "$o", "&ocirc;", "$u", "&ucirc;", "$e", "&ecirc;", "$a", "&acirc;", "$e", "&ecirc;", ")a", "&agrave;", ")e", "&egrave;", ")i", "&igrave;", ")o", "&ograve;", ")u", "&ugrave;", "_o", "&#x14d;", "+h", "&#x2b0;", ",c", "&#x255;", ",n", "&#x14b;", "''", "&#x294;", "'", "&rsquo;", "$h", "&#x2b1;", "-i", "&#x268;", "=j", "&#x25f;", "$l", "&#x28e;", "$n", "&#x272;", "$r", "&#x279;", ",r", "&#x157;", "!e", "&#x259;", "-u", "&#x289;", "_u", "&#x16b;"]
+  var terms = new Array;
+  for (termnum in arr) {
+    var term = arr[termnum];
+    for (i = 0; i < marking.length; i++) {
+      term = term.split(marking[i]).join(marking[++i]);
+    }
+    terms.push(term);
+  }
+  return terms;
 }
+
 
 // displays results as list
 // @param Array arr: results array
 function display(arr, data, id, terms) {
-	terms = markdown(terms);
-	if (!arr.length) {
-		document.getElementById(id).innerHTML = "Search term(s) not found";
-		return;
-	}
-	var text = "<ol>"
-	for (var pagenum in arr) {
-		var page = arr[pagenum];
-		var link = data.urls[page[0]] + ".html";
-		var name = data.names[page[0]];
-		var lines = new Array;
-		for (linenum in page[1]) {
-			var line = data.sentences[page[1][linenum]];
-			for (termnum in terms) {
-				var term = terms[termnum];
-				var replacement = "<strong>" + term + "</strong>"
-				line = line.split(term).join(replacement);
-				term = capitalise(term);
-				replacement = "<strong>" + term + "</strong>"
-				line = line.split(term).join(replacement);
-			}
-			lines.push(line);
-		}
-		text += "<li><a href=\"/" + link + "\">" + name + "</a>: "
-		text += lines.join(" &hellip; ") + "</li>"
-	}
-	text += "</ol>";
-	document.getElementById(id).innerHTML = text;
+  terms = markdown(terms);
+  if (arr.length == 0) {
+    document.getElementById(id).innerHTML = "<ul><li>" + terms.join(" ") + " not found</li></ul>";
+    return;
+  }
+  var text = "<ol>"
+  for (var pagenum in arr) {
+    var page = arr[pagenum];
+    var link = data.urls[page[0]] + ".html";
+    var name = data.names[page[0]];
+    var lines = new Array;
+    for (linenum in page[1]) {
+      var line = data.sentences[page[1][linenum]];
+      for (termnum in terms) {
+        var term = terms[termnum];
+        var replacement = "<strong>" + term + "</strong>"
+        line = line.split(term).join(replacement);
+        term = capitalise(term);
+        replacement = "<strong>" + term + "</strong>"
+        line = line.split(term).join(replacement);
+      }
+      lines.push(line);
+    }
+    text += "<li><a href=\"/" + link + "\">" + name + "</a>: "
+    text += lines.join(" &hellip; ") + "</li>"
+  }
+  text += "</ol>";
+  document.getElementById(id).innerHTML = text;
 }
